b05047e220795fb138397b9ff180a6f4b5b643cc
Evan Sosenko <razorx@evansosenko.com>
2016-05-18 13:55:31 -0700

    Add pytest

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+from sys import argv
+
 from setuptools import find_packages, setup
 
 from tensorflow_tutorials import __version__
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(argv)
+pytest_runner = ['pytest-runner>=2.7.1,<3.0.0'] if needs_pytest else []
 
 setup(
     name='tensorflow-tutorials',
@@ -15,8 +20,11 @@ setup(
     license='MIT',
     description='Tutorials for TensorFlow.',
     long_description=long_description,
+    tests_require=[
+        'pytest>=2.9.1,<3.0.0',
+    ],
     install_requires=[
         'numpy>=1.10.0,<2.0.0',
         'tensorflow>=0.8.0,<1.0.0'
-    ]
+    ] + pytest_runner
 )
