85384cb811c8e4cfdcaa1c207ac2274f352b86e9
Evan Sosenko <razorx@evansosenko.com>
2016-05-19 13:22:14 -0700

    Add source link to docstring

--- tensorflow_tutorials/__init__.py
+++ tensorflow_tutorials/__init__.py
@@ -1,6 +1,9 @@
 """
 [Tutorials] for [TensorFlow].
 
+[Source on GitHub][source].
+
+[source]: https://github.com/rxedu/tensorflow-tutorials
 [Tutorials]: https://www.tensorflow.org/versions/r0.8/tutorials/index.html
 [TensorFLow]: https://www.tensorflow.org/
 """
