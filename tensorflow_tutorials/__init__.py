cb46dffe0ee0079dd71064aef5b34dc1fabdedf0
Evan Sosenko <razorx@evansosenko.com>
2016-05-20 18:52:49 -0700

    Remove version import from __init__

--- tensorflow_tutorials/__init__.py
+++ tensorflow_tutorials/__init__.py
@@ -7,7 +7,6 @@
 [Tutorials]: https://www.tensorflow.org/versions/r0.8/tutorials/index.html
 [TensorFLow]: https://www.tensorflow.org/
 """
-from .version import __version__
 from .mnist import mnist
 
 __all__ = [
